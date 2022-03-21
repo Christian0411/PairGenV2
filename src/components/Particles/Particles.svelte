bb81fb7c71c260bc8cafd99b6079b2592bb30893
Christian0411 <cristian041195@gmail.com>
2022-03-20 21:46:20 -0400

    Add background particles

---
+++ src/components/Particles/Particles.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { pairs } from '../../store/teamStore';
+
+	import { onMount } from 'svelte';
+
+	let ParticlesComponent;
+
+	onMount(async () => {
+		const module = await import('svelte-particles');
+
+		ParticlesComponent = module.default;
+	});
+
+	let particlesConfig = {
+		particles: {
+			number: {
+				value: $pairs.length > 1 ? 50 : 0
+			},
+			fullscreen: {
+				zIndex: -1
+			},
+			color: {
+				value: '#56ca8d'
+			},
+			links: {
+				enable: true,
+				color: '#56ca8d'
+			},
+			move: {
+				enable: true
+			}
+		}
+	};
+
+	let onParticlesLoaded = (event) => {
+		const particlesContainer = event.detail.particles;
+
+		// you can use particlesContainer to call all the Container class
+		// (from the core library) methods like play, pause, refresh, start, stop
+	};
+
+	let onParticlesInit = (main) => {
+		// you can use main to customize the tsParticles instance adding presets or custom shapes
+	};
+</script>
+
+<svelte:component
+	this={ParticlesComponent}
+	id="tsparticles"
+	options={particlesConfig}
+	on:particlesLoaded={onParticlesLoaded}
+	on:particlesInit={onParticlesInit}
+/>
