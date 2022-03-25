1b62f1e374082b4bfa30d1bad45f727a0ce7962f
Christian0411 <cristian041195@gmail.com>
2022-03-25 00:12:08 -0400

    PairCard: slight fade in and out transition

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import { pairs, hotKeyPressed, generatePairs } from '../store/teamStore.js';
+	import { fade, fly } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+	import { pairs, hotKeyPressed, generatePairs, members } from '../store/teamStore.js';
 	import MembersPanel from '@components/MembersPanel/MembersPanel.svelte';
 	import PairCard from '@components/PairCard/PairCard.svelte';
 	import * as storage from '../storage/storage';
@@ -13,8 +15,10 @@
 	<MembersPanel on:click={generatePairs} />
 
 	<div class="flex ml-10 max-w-screen-lg flex-wrap h-1">
-		{#each $pairs as pair, i}
-			<PairCard {pair} id={i} />
+		{#each $pairs as pair, i (i)}
+			<div in:fade={{ duration: 100 }} out:fade={{ duration: 100 }}>
+				<PairCard {pair} id={i} />
+			</div>
 		{/each}
 		<div />
 	</div>
